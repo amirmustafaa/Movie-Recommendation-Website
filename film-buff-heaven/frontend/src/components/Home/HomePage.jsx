39eac78b4fa38b14e3e8d1dbd75eb3e47ea355c1
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-12 19:55:45 -0400

    Add top rated movies

--- film-buff-heaven/frontend/src/components/Home/HomePage.jsx
+++ film-buff-heaven/frontend/src/components/Home/HomePage.jsx
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import {
+  fetchMovies,
+  fetchGenre,
+  fetchMovieByGenre,
+  fetchPersons,
+  fetchTopratedMovie,
+} from "../../service/service.js";
+
+import { Link } from "react-router-dom";
+
+
 
 
 function HomePage() {
+  
+  //const [nowPlaying, setNowPlaying] = useState([]);
+  const [genres, setGenres] = useState([]);
+  const [movieByGenre, setMovieByGenre] = useState([]);
+  const [persons, setPersons] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      //setNowPlaying(await fetchMovies());
+      setGenres(await fetchGenre());
+      setMovieByGenre(await fetchMovieByGenre(28));
+      setPersons(await fetchPersons());
+      setTopRated(await fetchTopratedMovie());
+    };
+
+    fetchAPI();
+  }, []);
+
+
+  const topRatedList = topRated.slice(0, 4).map((item, index) => {
     return (
-      <div>
-        <h1>Test</h1>
+      <div className="col-md-3" key={index}>
+        <div className="card">
+          <Link to={`/movie/${item.id}`}>
+            <img className="img-fluid" src={item.poster} alt={item.title}></img>
+          </Link>
+        </div>
+        <div className="mt-3">
+          <p style={{ fontWeight: "bolder" }}>{item.title}</p>
+          <p>Rated: {item.rating}</p>
+          
+        </div>
       </div>
     );
+  });
+
+  return topRatedList;
   }
   
   export default HomePage;
