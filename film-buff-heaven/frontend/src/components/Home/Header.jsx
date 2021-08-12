a4fad7b8c1a434dd63c7dbe85e420c7dbc0eb73d
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-12 17:17:55 -0400

    Edit Header

--- film-buff-heaven/frontend/src/components/Home/Header.jsx
+++ film-buff-heaven/frontend/src/components/Home/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React  from 'react';
 
 function Header() {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    );
+    return(
+        <header className="masthead text-white text-center">
+          <div className="overlay"></div>
+          <div className="container">
+            <div className="row">
+              <div className="col-xl-9 mx-auto">
+                <h1 className="mb-5">Test</h1>
+              </div>
+              
+            </div>
+          </div>
+        </header>
+      );
+
+
   }
   
+
+//   <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
+//                 <div className="col-12 col-md-3 mx-auto">
+//                   <a href= "/register"><button type="submit"className="btn btn-block btn-lg btn-primary">Sign up!</button></a>
+//                 </div>
+//     </div>
   export default Header;
   
