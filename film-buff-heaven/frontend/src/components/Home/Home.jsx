abb151e6eb120d87ae75658a33430caa0ae8b2c8
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-12 17:03:34 -0400

    Add Header

--- film-buff-heaven/frontend/src/components/Home/Home.jsx
+++ film-buff-heaven/frontend/src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navbar from '../Navbar.jsx';
+import Header from './Header.jsx';
 import HomePage from './HomePage.jsx';
+
 
 function Home() {
     return (
       <div>
         <Navbar />
+        <Header />
         <HomePage />
       </div>
     );
