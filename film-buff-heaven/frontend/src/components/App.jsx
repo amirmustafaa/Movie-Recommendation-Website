a6c023e341be47c2b313214c7b9bbf5a3299a844
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-12 19:38:07 -0400

    Edit Service File

--- film-buff-heaven/frontend/src/components/App.jsx
+++ film-buff-heaven/frontend/src/components/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Home/Home.jsx';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Switch>
+          <Route path= "/" exact component = {Home} />
+        </Switch>  
+
+    </Router>
     </div>
   );
 }
