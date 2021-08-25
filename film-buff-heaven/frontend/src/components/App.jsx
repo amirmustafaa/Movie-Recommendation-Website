36fa3386dc535ef19d26d050cfa57065382db76e
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-25 13:45:23 -0400

    Add Information Page

--- film-buff-heaven/frontend/src/components/App.jsx
+++ film-buff-heaven/frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Home/Home.jsx';
+import Information from './Information/Information.jsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch>
           <Route path= "/" exact component = {Home} />
+          <Route path= "/information" exact component = {Information} />
         </Switch>  
 
     </Router>
---
+++ film-buff-heaven/frontend/src/components/Information/Information.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar.jsx';
+
+function Information() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+  
+  export default Information;
