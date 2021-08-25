40748b133ecac01bfb2bf1fd42aa1bf902d55133
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-25 16:43:44 -0400

    Edit Information Page

--- film-buff-heaven/frontend/src/components/Information/InformationPage.jsx
+++ film-buff-heaven/frontend/src/components/Information/InformationPage.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchMovieDetail } from '../../service/service';
+
 
 
 function InformationPage() {
-    return (
-      <div>
-        <h1>Test</h1>
-      </div>
-    );
+  const [movies, setMovies] = useState([]);
+  
+  useEffect(() => {
+    const fetchAPI = async () => {
+      //setNowPlaying(await fetchMovies());
+      setMovies(await fetchMovieDetail());
+      
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+
   }
   
   export default InformationPage;
