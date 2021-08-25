3c17d67dce1e0a0e5775a6f1f546f14b096267bf
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-08-25 14:52:42 -0400

    Add Login and Register Pages

--- film-buff-heaven/frontend/src/components/App.jsx
+++ film-buff-heaven/frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Home/Home.jsx';
 import Information from './Information/Information.jsx';
+import Login from './Login/Login.jsx';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path= "/" exact component = {Home} />
           <Route path= "/information" exact component = {Information} />
+          <Route path= "/login" exact component = {Login} />
         </Switch>  
 
     </Router>
--- film-buff-heaven/frontend/src/components/Login/Login.jsx
+++ film-buff-heaven/frontend/src/components/Login/Login.jsx
@@ -2,5 +2,15 @@ import React from 'react';
 import Navbar from '../Navbar.jsx';
 import LoginPage from './LoginPage.jsx';
 
-
+function Login() {
+    return (
+      <div>
+        <Navbar />
+        <LoginPage />
+      </div>
+    );
+  }
+  
+  export default Login;
+  
 
