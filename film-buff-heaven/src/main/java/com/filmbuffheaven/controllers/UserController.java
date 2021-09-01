60f953ab766c43350bc7ce259226654c19c1c8ff
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-01 19:05:44 -0400

    Add User Controller

---
+++ film-buff-heaven/src/main/java/com/filmbuffheaven/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.filmbuffheaven.controllers;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/test")
+public class UserController {
+
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	public String userAccess() {
+		UserDetails userDetails =
+				(UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		return userDetails.getUsername();
+	}
+
+
+}
