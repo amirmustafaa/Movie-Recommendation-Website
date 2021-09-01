35b605ede534102a7672c2787d3b5c0d3c1deeeb
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-01 19:03:54 -0400

    Edit App file

--- film-buff-heaven/frontend/src/components/App.jsx
+++ film-buff-heaven/frontend/src/components/App.jsx
@@ -1,11 +1,45 @@
 import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Axios from 'axios';
+import Cookies from 'universal-cookie';
+import UserContext from "../context/context.js";
 import Home from './Home/Home.jsx';
 import Information from './Information/Information.jsx';
 import Login from './Login/Login.jsx';
 import Register from './Register/Register.jsx';
 
 function App() {
+  const cookies = new Cookies();
+
+
+  const [userData, setUserData] = useState({
+    token:undefined,
+    user: undefined
+  })
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = cookies.get("auth-token");
+      if (token === undefined) {
+        cookies.set("auth-token", "", { path: '/' }, {httpOnly:true});
+        token = "";
+      }
+
+        const userRes = await Axios.get("https://www.moneyportalpro.com/api/test/user", {
+          headers: { "Authorization":  `Bearer ${token}`},
+        });
+        if(userRes.data){
+          setUserData({
+            token,
+            user: userRes.data,
+          });
+      }
+
+    };
+
+    checkLoggedIn();
+  }, []);
+
   return (
     <div>
       <Router>
