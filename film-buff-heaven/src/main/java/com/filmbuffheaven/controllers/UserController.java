34b4a26c008643f05d8f87851518a71897a3a3c6
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-01 19:08:21 -0400

    Add User Controller

--- film-buff-heaven/src/main/java/com/filmbuffheaven/controllers/UserController.java
+++ film-buff-heaven/src/main/java/com/filmbuffheaven/controllers/UserController.java
@@ -1,5 +1,4 @@
 package com.filmbuffheaven.controllers;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
