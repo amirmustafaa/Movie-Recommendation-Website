d74138903b7f0a8c5c55cfe9ac904c1c55f34ae4
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-02 09:56:59 -0400

    Edit Navbar

--- film-buff-heaven/frontend/src/components/Navbar.jsx
+++ film-buff-heaven/frontend/src/components/Navbar.jsx
@@ -16,15 +16,26 @@ import Logo from './logo.png';
 
 
 function NavBar(){
-    // const cookies = new Cookies();
+     const cookies = new Cookies();
      let history = useHistory();
 
-    // function logout() {
-    //     cookies.set("auth-token", "", { path: '/' }, {httpOnly:true});
-    //     history.replace("/");
-    // }
+    function logout() {
+        cookies.set("auth-token", "", { path: '/' }, {httpOnly:true});
+        history.replace("/");
+    }
 
-    let num =Math.floor(Math.random() * 500)
+    
+
+    let movieArr = [278, 238, 424, 240, 129, 496243, 497, 389, 680, 155, 122, 13, 11216, 637, 
+                    429, 346, 311, 539, 550, 324857, 429617, 399566, 557, 527774, 475557,578701, 
+                    672, 671, 274, 578, 44214, 1678, 3035, 28, 490132, 8587, 600354, 10098,
+                    186, 629, 1124, 857, 50014, 11324, 289, 124, 229, 1422, 925, 68718,7345, 
+                    901, 3082, 105, 11104, 106646, 3034, 313369, 262];
+
+    let rand =Math.floor(Math.random() * movieArr.length -1);
+
+    let num = movieArr[rand];
+
 
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
@@ -35,9 +46,9 @@ function NavBar(){
                 <Link className = "nav-buttons" to={'/login'}>
                     <NavbarText ><Button  color="primary">Sign In</Button>{' '}</NavbarText>
                 </Link>
-                <Link  to={`/information/${num}`}>
-                    <NavbarText ><Button  color="primary">Random Movie</Button>{' '}</NavbarText>
-                </Link>
+            
+                <NavbarText ><Button onClick={() => {window.location.href="/information/" + num}} color="primary">Random Movie</Button>{' '}</NavbarText>
+                
             </Navbar>
         </div>
 
