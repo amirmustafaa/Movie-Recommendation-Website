b3e6d0cf7e546b2d527dbca7ccee83f12aad27d0
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-09 23:27:15 -0400

    Edit Create List Page

--- film-buff-heaven/frontend/src/components/Lists/CreateListPage.jsx
+++ film-buff-heaven/frontend/src/components/Lists/CreateListPage.jsx
@@ -1,18 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext} from "react";
 import Axios from 'axios';
+import UserContext from "../../context/context"
 import {
     fetchMovieSearch
   } from "../../service/service.js";
 
 function CreatListPage() {
-
+    
+    const {userData} = useContext(UserContext);
+    const token = userData.token;
     const [state, setState] = useState({
         moviename: "",
+        listname: ""
       });
 
-    const [name, setName] = useState();
    
-    const [movie, setMovie] = useState([]);
+    const [movie, setMovie] = useState("");
 
     const [list, setList] = useState([])
 
@@ -33,12 +36,33 @@ function CreatListPage() {
 
     const createList = async () =>{
 
+        
         const listObject = {
+            name: state.listname,
             entries: list,
-            name: name,
           };
 
-        const ListRes = await Axios.post("http://localhost:8080/api/data/createList", listObject);
+        const listRes = await Axios.post("http://localhost:8080/api/data/createlist", listObject, {
+            headers: { "Authorization":  `Bearer ${token}`},
+          });
+
+        console.log(listRes);
+
+
+    }
+
+    const getData = async () =>{
+
+        
+        const listObject = {
+            listId: 1,
+          };
+
+        const dataRes = await Axios.post("http://localhost:8080/api/data/getlist", listObject, {
+            headers: { "Authorization":  `Bearer ${token}`},
+          });
+
+        console.log(dataRes);
 
 
     }
@@ -51,16 +75,18 @@ function CreatListPage() {
         <li><img width="100" height="150" src ={number} alt="Movie Poster"></img></li>
     );
 
-
+    
 
     return(
         <div>
             <ol>{listItems}</ol>
             <div className="searchbar md-form active-cyan-2 mb-3">
+            <input name = "listname" onChange = {handleChange} className="form-control" type="text" placeholder="List Name" aria-label="List Name"/>
                  <input name = "moviename" onChange = {handleChange} className="form-control" type="text" placeholder="Search" aria-label="Search"/>
              </div>
              <div className="row justify-content-center my-3 px-3"> <button type = "submit" onClick= {fetchAPI} className="btn-block btn-color">Add Movie</button> </div>
              <div className="row justify-content-center my-3 px-3"> <button type = "submit" onClick = {createList} className="btn-block btn-color">Create List</button> </div>
+             <div className="row justify-content-center my-3 px-3"> <button type = "submit" onClick = {getData} className="btn-block btn-color">Get Data</button> </div>
         </div>
     );
 }
