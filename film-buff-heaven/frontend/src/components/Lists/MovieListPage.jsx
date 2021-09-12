57c937f212e0b27275ac360d1a1998243e2d1bcb
Amir Mustafaa <amirmustafaa1@gmail.com>
2021-09-11 22:17:53 -0400

    Edit List Page

--- film-buff-heaven/frontend/src/components/Lists/MovieListPage.jsx
+++ film-buff-heaven/frontend/src/components/Lists/MovieListPage.jsx
@@ -1,9 +1,43 @@
-import React, {useContext} from 'react';
-import UserContext from '../../context/context.js';
+import React, {useState, useContext, useEffect} from 'react';
+import UserContext from "../../context/context";
+import { Link } from "react-router-dom";
+import {useLocation } from 'react-router';
+import Axios from 'axios';
+import Cookies from 'universal-cookie';
 
 
 function MovieList (){
+    const cookies = new Cookies();
     const {userData} = useContext(UserContext);
+    const token = userData.token;
+    const location = useLocation();
+    const listId = location.state.data;
+
+    const [dropdown, setDropdown] = useState(false);
+    const toggleOpen = () => setDropdown(!dropdown);
+
+    const [listState, setListState] = useState([]);
+
+
+    const getData = async () =>{
+        
+        const listObject = {
+            listId: listId
+        };
+
+        const dataRes = await Axios.post("http://localhost:8080/api/data/getlistdata",listObject,{
+            headers: { "Authorization":  `Bearer ${token}`},
+          });
+
+        console.log(dataRes.data);
+
+       
+    }
+
+    useEffect(() => {
+        getData();
+      },[])
+
     return(
         <div>
             <h1>Test</h1>
